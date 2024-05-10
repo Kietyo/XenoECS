1620d8b987bcbeec87c20af3356ded0465e3194e
Kiet <5054996+Kietyo@users.noreply.github.com>
2024-05-09 21:52:38 -0700

    IWorld ECS

---
+++ src/commonMain/kotlin/com/xenotactic/ecs/IWorld.kt
@@ -0,0 +1,24 @@
+package com.xenotactic.ecs
+
+import kotlin.reflect.KClass
+
+interface IWorld {
+    fun containsEntity(entityId: EntityId): Boolean
+    operator fun <T : Any> get(entityId: EntityId, kClass: KClass<T>): T
+    fun <T : Any> getOrNull(entityId: EntityId, kClass: KClass<T>): T?
+    fun getEntities(
+        familyConfiguration: FamilyConfiguration
+    ): Set<EntityId>
+    fun getStatefulEntities(): List<StatefulEntity>
+    fun getStatefulEntitySnapshots(
+        familyConfiguration: FamilyConfiguration
+    ): List<StatefulEntity>
+    fun getStatefulEntitySnapshot(
+        entityId: EntityId
+    ): StatefulEntity
+    fun getFirstStatefulEntityMatching(familyConfiguration: FamilyConfiguration): StatefulEntity
+    fun getStagingEntities(): List<StagingEntity>
+    fun getStagingEntity(
+        entityId: EntityId
+    ): StagingEntity
+}
---
+++ src/commonTest/kotlin/com/xenotactic/ecs/SealedClassInjectionTest.kt
@@ -0,0 +1,46 @@
+package com.xenotactic.ecs
+
+import com.kietyo.ktruth.assertThat
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+interface State
+
+sealed interface SealedClassState: State {
+    data class StringState(val test: String): SealedClassState
+    data class IntState(val test: Int): SealedClassState
+}
+
+internal class SealedClassInjectionTest {
+    @Test
+    fun setSealedClassChildren_canGetSealedClassChildren() {
+        val injections = TypedInjections<State>()
+
+        val stringState = SealedClassState.StringState("test")
+        injections.setSingletonOrThrow<Any>(stringState)
+        val intState = SealedClassState.IntState(123)
+        injections.setSingletonOrThrow<Any>(intState)
+
+        assertEquals(stringState, injections.getSingleton<SealedClassState.StringState>())
+        assertEquals(intState, injections.getSingleton<SealedClassState.IntState>())
+
+        val thrown = assertFails {
+            injections.getSingleton<SealedClassState>()
+        }
+        assertThat(thrown.message!!).contains("Singleton injection does not exist: ")
+    }
+
+    @Test
+    fun sealedChild_castedAsParent_stillCannotGetUsingParentType() {
+        val injections = TypedInjections<State>()
+
+        val intState = SealedClassState.IntState(123) as SealedClassState
+        injections.setSingletonOrThrow<SealedClassState>(intState)
+
+        val thrown = assertFails {
+            injections.getSingleton<SealedClassState>()
+        }
+        assertThat(thrown.message!!).contains("Singleton injection does not exist: ")
+    }
+}
